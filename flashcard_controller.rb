3a00debbc4c399d2ac86d46f5288b14a9a38a542
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 16:34:18 -0500

    controller working

--- flashcard_controller.rb
+++ flashcard_controller.rb
@@ -25,35 +25,7 @@ class FlashcardController
     end
     @view.display_game_over
   end
-
-  def get_card_from_deck
-    @play.get_next_card
-  end
-
-
-  def show_definition_to_user
-  end
-
-  def get_user_answer
-  end
-
-
-  def ask_deck_if_user_answer_is_correct
-  end
-
-  def handle_deck_response_to_user_input
-  end
-
-  def show_deck_response_to_user
-  end
-
-  def show_deck_response_to_user
-  end
-
-
-
-
-
-end 
+end
+  
 
 FlashcardController.new.play_cards
